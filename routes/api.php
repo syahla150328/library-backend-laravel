51db5aae621eeb29326918ae035558bb6ca96588
syahla1503 <oss566637@gmail.com>
2026-01-27 20:23:20 -0800

    menambahkan API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Endpoint Public (Bisa diakses tanpa login)
+Route::post('/login', [AuthController::class, 'login']);
+
+// Endpoint Protected (Harus login/bawa token)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Nanti route buku & transaksi taruh di sini
+});
